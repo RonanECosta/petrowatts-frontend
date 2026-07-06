1177686158cb6e4fa0c4fdf85367b7c9c6b81cfc
Ronan E. Costa <ronan.costa@serpro.gov.br>
2026-07-06 14:23:36 -0300

    feat: alterações diversas

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const API_BASE_URL = 'http://127.0.0.1:5000';
 // Inicializa a página
 document.addEventListener('DOMContentLoaded', () => {
     carregarUfs();
-    carregarFabricantes();
+    carregarFabricantesCombustao();
 });
 
 // Alimenta o combo de UFs dinamicamente
@@ -24,8 +24,7 @@ const carregarUfs = () => {
     });
 };
 
-// Busca todas as marcas (fabricantes) do banco de dados
-const carregarFabricantes = async () => {
+const carregarFabricantesCombustao = async () => {
     const selectFabricante = document.getElementById('fabricante');
 
     try {
@@ -97,7 +96,6 @@ document.getElementById('modelo').addEventListener('change', async (e) => {
     }
 });
 
-// Função chamada pelo clique do botão "Cadastrar" (mapeado como novoCadastro no HTML)
 const novoCadastro = async () => {
     let cpf = document.getElementById("cpf").value;
     let nome = document.getElementById("nome").value;
@@ -110,7 +108,6 @@ const novoCadastro = async () => {
     let idFabricante = comboFabricante.value; // Captura o ID numérico (ex: 2)
     let fabricante = comboFabricante.options[comboFabricante.selectedIndex]?.text || ''; // Captura o texto (ex: "Fiat")
 
-    // Validação
     if (!cpf.trim() || !nome.trim() || !uf || !idFabricante || !modelo || !ano || !rodagem) {
         alert("Por favor, preencha todos os campos obrigatórios antes de continuar!");
         return;
@@ -128,19 +125,46 @@ const novoCadastro = async () => {
 
     const data = await response.json();
 
-    //popula campos na index com os dados recém-cadastrados
-    document.getElementById('resId').innerText = data.id_usuario;
-    document.getElementById('resNome').innerText = nome;
-    document.getElementById('resUf').innerText = uf;
-    document.getElementById('resFabricante').innerText = fabricante;
-    document.getElementById('resModelo').innerText = modelo;
-    document.getElementById('resAno').innerText = parseInt(ano, 10);
-    document.getElementById('resKm').innerText = parseInt(rodagem, 10);
-
-    //resetaFormulario();
+    populaTela(data.id_usuario, nome, uf, fabricante, modelo, parseInt(ano, 10), parseInt(rodagem, 10));
 };
 
-// Função chamada pelo clique do botão "Buscar" (mapeado como novaConsulta no HTML)
+const alterarCadastro = async () => {
+    let id_usuario = document.getElementById("resId").innerText;
+    if (!id_usuario || id_usuario === "-") {
+        alert("Nenhum usuário selecionado para alteração.");
+        return;
+    }
+    let cpf = document.getElementById("cpf").value;
+    let nome = document.getElementById("nome").value;
+    let uf = document.getElementById("uf").value;
+    let rodagem = document.getElementById("rodagemMensal").value;
+    let modelo = document.getElementById("modelo").value;
+    let ano = document.getElementById("anoFabricacao").value;
+
+    let comboFabricante = document.getElementById("fabricante");
+    let idFabricante = comboFabricante.value; // Captura o ID numérico (ex: 2)
+    let fabricante = comboFabricante.options[comboFabricante.selectedIndex]?.text || ''; // Captura o texto (ex: "Fiat")
+
+    if (!cpf.trim() || !nome.trim() || !uf || !idFabricante || !modelo || !ano || !rodagem) {
+        alert(`Por favor, preencha todos os campos obrigatórios antes de continuar! Campos obrigatórios: ${!cpf.trim() ? 'CPF, ' : ''}${!nome.trim() ? 'Nome, ' : ''}${!uf ? 'UF, ' : ''}${!idFabricante ? 'Fabricante, ' : ''}${!modelo ? 'Modelo, ' : ''}${!ano ? 'Ano, ' : ''}${!rodagem ? 'Rodagem Mensal' : ''}`);
+        return;
+    }
+
+    const response = await patchItem(id_usuario, cpf, nome, uf, idFabricante, modelo, ano, rodagem);
+
+    console.info(`Resposta do servidor para o cadastro de CPF ${cpf}: Status ${response.status}`);
+    if (response.status != 200) {
+        alert("Erro no cadastro. Verifique os dados e tente novamente.");
+        return;
+    }
+
+    alert("Cadastro e vínculo realizados com sucesso!");
+
+    const data = await response.json();
+
+    populaTela(id_usuario, nome, uf, fabricante, modelo, parseInt(ano, 10), parseInt(rodagem, 10));
+};
+
 const novaConsulta = async () => {
     let cpf = document.getElementById("cpf").value;
 
@@ -158,13 +182,8 @@ const novaConsulta = async () => {
     const data = await response.json();
 
     // Atualiza campos com os dados que vieram do servidor
-    document.getElementById('resId').innerText = data.id_usuario;
-    document.getElementById('resNome').innerText = data.nome;
-    document.getElementById('resUf').innerText = data.estado;
-    document.getElementById('resFabricante').innerText = data.veiculo.fabricante;
-    document.getElementById('resModelo').innerText = data.veiculo.modelo;
-    document.getElementById('resAno').innerText = parseInt(data.veiculo.ano, 10);
-    document.getElementById('resKm').innerText = parseInt(data.veiculo.km_mensal, 10);
+    populaFormulario(data);
+    populaTela(data.id_usuario, data.nome, data.estado, data.veiculo.fabricante, data.veiculo.modelo, data.veiculo.ano, data.veiculo.km_mensal);
 };
 
 const postItem = async (cpf, nome, uf, idFabricante, modelo, ano, kmMensal) => {
@@ -195,24 +214,7 @@ const postItem = async (cpf, nome, uf, idFabricante, modelo, ano, kmMensal) => {
     }
 };
 
-const resetaCamposConsulta = () => {
-    document.getElementById('resId').innerText = "-";
-    document.getElementById('resNome').innerText = "-";
-    document.getElementById('resUf').innerText = "-";
-    document.getElementById('resFabricante').innerText = "-";
-    document.getElementById('resModelo').innerText = "-";
-    document.getElementById('resAno').innerText = "-";
-    document.getElementById('resKm').innerText = "-";
-};
-
-const alterarCadastro = async () => {
-    let id_usuario = document.getElementById("resId").innerText;
-
-    if (!id_usuario || id_usuario === "-") {
-        alert("Nenhum usuário selecionado para alteração.");
-        return;
-    }
-
+const patchItem = async (id_usuario, cpf, nome, uf, idFabricante, modelo, ano, kmMensal) => {
     const formData = new FormData();
     formData.append('id_usuario', id_usuario);
     formData.append('cpf', cpf);
@@ -261,14 +263,71 @@ const deleteCadastro = async () => {
     const data = await response.json();
 
     alert(data.message);
-    resetaCamposConsulta();
-    resetaFormulario();
+    limpaTela();
+    limpaFormulario();
     document.getElementById("deleteBtn").disabled = true;
 
 };
 
-// Limpa os campos após inserção bem sucedida
-const resetaFormulario = () => {
+const populaFormulario = async (data) => {
+    try {
+        document.getElementById("cpf").value = data.cpf || "";
+        document.getElementById("nome").value = data.nome || "";
+        document.getElementById("uf").value = data.estado || "";
+        document.getElementById("rodagemMensal").value = data.veiculo.km_mensal || "";
+
+        //seleciona o fabricante do veículo no combo de fabricantes
+        const selectFabricante = document.getElementById("fabricante");
+        selectFabricante.value = data.veiculo.id_fabricante || "";
+        selectFabricante.dispatchEvent(new Event('change'));//Dispara o evento de mudança para carregar os modelos correspondentes
+        await new Promise(resolve => setTimeout(resolve, 300)); //aguarda o retorno
+
+        //seleciona o modelo do veículo no combo de modelos
+        const selectModelo = document.getElementById("modelo");
+        selectModelo.value = data.veiculo.modelo || "";
+        selectModelo.dispatchEvent(new Event('change'));//Dispara o evento de mudança para carregar os anos do modelo correspondente
+        await new Promise(resolve => setTimeout(resolve, 300)); //aguarda o retorno
+
+        //seleciona o ano do veículo no combo de anos
+        const selectAno = document.getElementById("anoFabricacao");
+        selectAno.value = parseInt(data.veiculo.ano, 10) || "";
+
+    } catch (error) {
+        console.error("Erro ao popular o formulário:", error);
+    }
+};
+
+function populaTela(data) {
+    populaTela(data.id_usuario, data.nome, data.estado, data.veiculo.fabricante, data.veiculo.modelo, data.veiculo.ano, data.veiculo.km_mensal);
+}
+
+function populaTela(id_usuario, nome, estado, fabricante, modelo, ano, km_mensal) {
+    document.getElementById('resId').innerText = id_usuario;
+    document.getElementById('resNome').innerText = nome;
+    document.getElementById('resUf').innerText = estado;
+    document.getElementById('resFabricante').innerText = fabricante;
+    document.getElementById('resModelo').innerText = modelo;
+    document.getElementById('resAno').innerText = parseInt(ano, 10);
+    document.getElementById('resKm').innerText = parseInt(km_mensal, 10);
+
+    document.getElementById("deleteBtn").disabled = false;
+    document.getElementById("alterarBtn").disabled = false;
+}
+
+const limpaTela = () => {
+    document.getElementById('resId').innerText = "-";
+    document.getElementById('resNome').innerText = "-";
+    document.getElementById('resUf').innerText = "-";
+    document.getElementById('resFabricante').innerText = "-";
+    document.getElementById('resModelo').innerText = "-";
+    document.getElementById('resAno').innerText = "-";
+    document.getElementById('resKm').innerText = "-";
+
+    document.getElementById("deleteBtn").disabled = true;
+    document.getElementById("alterarBtn").disabled = true;
+};
+
+const limpaFormulario = () => {
     document.getElementById("cpf").value = "";
     document.getElementById("nome").value = "";
     document.getElementById("uf").selectedIndex = 0;
@@ -278,4 +337,8 @@ const resetaFormulario = () => {
     document.getElementById("anoFabricacao").innerHTML = '<option value="" disabled selected>Ano</option>';
     document.getElementById("anoFabricacao").disabled = true;
     document.getElementById("rodagemMensal").value = "";
+
+    document.getElementById("deleteBtn").disabled = true;
+    document.getElementById("alterarBtn").disabled = true;
 };
+
